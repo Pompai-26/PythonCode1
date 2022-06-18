9f99d876df4a65f89391070d9e52d84d47d22a07
Pompai-26 <107717946+Pompai-26@users.noreply.github.com>
2022-06-18 15:50:28 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,25 @@
-print ('Hello')
+import PySimpleGUI as sg
+
+layout = [[sg.Text("Welcome to Moumita's World")], [sg.Text('Please enter your Name')],
+    [sg.Text('Name', size =(15, 1)), sg.InputText()], [sg.Text("", size=(15, 1), key='OUTPUT')], [sg.Button("OK")], [sg.Button("QUIT")], [sg.Image('D:\Alu\Test.png')]]
+# Create the window
+window = sg.Window("Demo", layout, margins=(100, 100))
+
+
+# Create an event loop
+while True:
+    event, values = window.read()
+    # End program if user closes window or
+    # presses the OK button
+    if event == "QUIT" or event == sg.WIN_CLOSED:
+        break
+    if event == "OK":
+        name = values[0]
+        window['OUTPUT'].update(value=name)
+
+        sg.popup('Hello', name, 'Welcome to My World')
+
+
+
+
+window.close()
